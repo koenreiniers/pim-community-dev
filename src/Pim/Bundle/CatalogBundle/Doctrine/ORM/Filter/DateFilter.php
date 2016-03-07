9bddcf45e558e93236dabe755222d2757242b9b8
Yohan BLAIN <yohan.blain@gmail.com>
2016-03-07 11:29:03 +0100

    Extract a DateTimeFilter from ORM DateFilter

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/DateFilter.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/DateFilter.php
@@ -3,7 +3,6 @@
 namespace Pim\Bundle\CatalogBundle\Doctrine\ORM\Filter;
 
 use Pim\Bundle\CatalogBundle\Query\Filter\AttributeFilterInterface;
-use Pim\Bundle\CatalogBundle\Query\Filter\FieldFilterInterface;
 use Pim\Bundle\CatalogBundle\Query\Filter\Operators;
 use Pim\Bundle\CatalogBundle\Validator\AttributeValidatorHelper;
 use Pim\Component\Catalog\Exception\InvalidArgumentException;
@@ -16,40 +15,26 @@ use Pim\Component\Catalog\Model\AttributeInterface;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class DateFilter extends AbstractAttributeFilter implements FieldFilterInterface, AttributeFilterInterface
+class DateFilter extends AbstractAttributeFilter implements AttributeFilterInterface
 {
+    const DATETIME_FORMAT = 'Y-m-d';
+
     /** @var array */
     protected $supportedAttributes;
 
-    /** @var array */
-    protected $supportedFields;
-
     /**
-     * Instantiate the base filter
-     *
      * @param AttributeValidatorHelper $attrValidatorHelper
      * @param array                    $supportedAttributes
-     * @param array                    $supportedFields
      * @param array                    $supportedOperators
      */
     public function __construct(
         AttributeValidatorHelper $attrValidatorHelper,
         array $supportedAttributes = [],
-        array $supportedFields = [],
         array $supportedOperators = []
     ) {
         $this->attrValidatorHelper = $attrValidatorHelper;
         $this->supportedAttributes = $supportedAttributes;
-        $this->supportedFields     = $supportedFields;
         $this->supportedOperators  = $supportedOperators;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function supportsField($field)
-    {
-        return in_array($field, $this->supportedFields);
     }
 
     /**
@@ -99,8 +84,8 @@ class DateFilter extends AbstractAttributeFilter implements FieldFilterInterface
             );
             $this->qb->andWhere(
                 $this->qb->expr()->orX(
-                    $this->qb->expr()->lt($backendField, $this->getDateLiteralExpr($value[0], true)),
-                    $this->qb->expr()->gt($backendField, $this->getDateLiteralExpr($value[1], true))
+                    $this->qb->expr()->lt($backendField, $this->qb->expr()->literal($value[0])),
+                    $this->qb->expr()->gt($backendField, $this->qb->expr()->literal($value[1]))
                 )
             );
         } else {
@@ -118,95 +103,6 @@ class DateFilter extends AbstractAttributeFilter implements FieldFilterInterface
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function addFieldFilter($field, $operator, $value, $locale = null, $scope = null, $options = [])
-    {
-        if (Operators::IS_EMPTY === $operator) {
-            $value = null;
-        } else {
-            $value = $this->formatValues($field, $value);
-        }
-
-        $field = current($this->qb->getRootAliases()) . '.' . $field;
-
-        switch ($operator) {
-            case Operators::BETWEEN:
-                $this->qb->andWhere(
-                    $this->qb->expr()->andX(
-                        $this->qb->expr()->gt($field, $this->getDateLiteralExpr($value[0])),
-                        $this->qb->expr()->lt($field, $this->getDateLiteralExpr($value[1], true))
-                    )
-                );
-                break;
-
-            case Operators::NOT_BETWEEN:
-                $this->qb->andWhere(
-                    $this->qb->expr()->orX(
-                        $this->qb->expr()->lt($field, $this->getDateLiteralExpr($value[0])),
-                        $this->qb->expr()->gt($field, $this->getDateLiteralExpr($value[1], true))
-                    )
-                );
-                break;
-
-            case Operators::GREATER_THAN:
-                $this->qb->andWhere($this->qb->expr()->gt($field, $this->getDateLiteralExpr($value, true)));
-                break;
-
-            case Operators::LOWER_THAN:
-                $this->qb->andWhere($this->qb->expr()->lt($field, $this->getDateLiteralExpr($value)));
-                break;
-
-            case Operators::EQUALS:
-                $this->qb->andWhere(
-                    $this->qb->expr()->andX(
-                        $this->qb->expr()->gt($field, $this->getDateLiteralExpr($value)),
-                        $this->qb->expr()->lt($field, $this->getDateLiteralExpr($value, true))
-                    )
-                );
-                break;
-
-            case Operators::IS_EMPTY:
-                $this->qb->andWhere($this->qb->expr()->isNull($field));
-                break;
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get the literal expression of the date
-     *
-     * @param string $data
-     * @param bool   $endOfDay
-     *
-     * @return \Doctrine\ORM\Query\Expr\Literal
-     */
-    protected function getDateLiteralExpr($data, $endOfDay = false)
-    {
-        return $this->qb->expr()->literal($this->getDateValue($data, $endOfDay));
-    }
-
-    /**
-     * Get the date formatted from data
-     *
-     * @param \DateTime|string $data
-     * @param bool             $endOfDay
-     *
-     * @return string
-     */
-    protected function getDateValue($data, $endOfDay = false)
-    {
-        if ($data instanceof \DateTime && true === $endOfDay) {
-            $data->setTime(23, 59, 59);
-        } elseif (!$data instanceof \DateTime && true === $endOfDay) {
-            $data = sprintf('%s 23:59:59', $data);
-        }
-
-        return $data instanceof \DateTime ? $data->format('Y-m-d H:i:s') : $data;
-    }
-
-    /**
      * Format values to string or array of strings
      *
      * @param string $type
@@ -219,7 +115,7 @@ class DateFilter extends AbstractAttributeFilter implements FieldFilterInterface
         if (is_array($value) && 2 !== count($value)) {
             throw InvalidArgumentException::expected(
                 $type,
-                'array with 2 elements, string or \Datetime',
+                'array with 2 elements, string or \DateTime',
                 'filter',
                 'date',
                 print_r($value, true)
@@ -248,13 +144,13 @@ class DateFilter extends AbstractAttributeFilter implements FieldFilterInterface
     protected function formatSingleValue($type, $value)
     {
         if ($value instanceof \DateTime) {
-            $value = $value->format('Y-m-d');
+            $value = $value->format(static::DATETIME_FORMAT);
         } elseif (is_string($value)) {
             $this->validateDateFormat($type, $value);
         } elseif (null !== $value) {
             throw InvalidArgumentException::expected(
                 $type,
-                'array with 2 elements, string or \Datetime',
+                'array with 2 elements, string or \DateTime',
                 'filter',
                 'date',
                 print_r($value, true)
@@ -272,15 +168,12 @@ class DateFilter extends AbstractAttributeFilter implements FieldFilterInterface
      */
     protected function validateDateFormat($type, $value)
     {
-        $dateValues = explode('-', $value);
+        $dateTime = \DateTime::createFromFormat(static::DATETIME_FORMAT, $value);
 
-        if (count($dateValues) !== 3
-            || (!is_numeric($dateValues[0]) || !is_numeric($dateValues[1]) || !is_numeric($dateValues[2]))
-            || !checkdate($dateValues[1], $dateValues[2], $dateValues[0])
-        ) {
+        if (!$dateTime || 0 < $dateTime->getLastErrors()['warning_count']) {
             throw InvalidArgumentException::expected(
                 $type,
-                'a string with the format yyyy-mm-dd',
+                'a string with the format ' . static::DATETIME_FORMAT,
                 'filter',
                 'date',
                 $value
