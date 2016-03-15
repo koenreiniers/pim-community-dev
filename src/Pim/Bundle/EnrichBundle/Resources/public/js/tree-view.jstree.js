ee6f69e88394181f8291a91788dd70b66ecce8a4
Olivier Soulet <olivier.soulet@akeneo.com>
2016-03-15 16:22:15 +0100

    temp

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/tree-view.jstree.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/tree-view.jstree.js
@@ -11,6 +11,7 @@ define(
             dataLocale    = null,
             relatedEntity = null,
             $el           = null,
+            categoryBaseRoute = '',
 
             getActiveNode = function(skipVirtual) {
                 if (skipVirtual) {
@@ -122,7 +123,7 @@ define(
                                 return {
                                     id: getNodeId(node),
                                     select_node_id: getActiveNode(),
-                                    with_products_count: 1,
+                                    with_products_count: 1, // TODO: Change naming
                                     include_sub: +includeSub
                                 };
                             }
@@ -202,7 +203,7 @@ define(
             };
 
         return {
-            init: function($element, state) {
+            init: function($element, state, baseRoute) {
                 if (!$element || !$element.length || !_.isObject($element)) {
                     return;
                 }
@@ -213,6 +214,7 @@ define(
                 selectedNode  = _.has(state, 'selectedNode') ? state.selectedNode : selectedNode;
                 selectedTree  = _.has(state, 'selectedTree') ? state.selectedTree : selectedTree;
                 includeSub    = _.has(state, 'includeSub')   ? state.includeSub   : includeSub;
+                categoryBaseRoute = baseRoute;
 
                 initTree();
             },
